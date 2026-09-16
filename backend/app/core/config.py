f8d600499a811846936c58a4b4dbfc1582a6de2d
ShahidaBegumSheik <shahida18@gmail.com>
2026-09-16 15:47:53 +0530

    added git attributes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str = "HS256"
 
     # Access Token
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10
 
     # Refresh Token
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
